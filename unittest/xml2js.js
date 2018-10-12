ec43de77568768db89ffd22536cb585db10c8935
i042416 <cle.ee@163.com>
2018-10-12 11:07:16 +0800

    exit hook

--- unittest/xml2js.js
+++ unittest/xml2js.js
@@ -9,11 +9,27 @@ text=text+"<year>2005</year>";
 text=text+"</book>";
 text=text+"</bookstore>";
 
+var a = require("./config");
+
+console.log(a.dev.name);
+console.log(a.prod.name);
+
+process.on('exit', (code) => {
+  console.log(`即将退出，退出码：${code}`);
+});
+
+process.on('uncaughtException', (err) => {
+  fs.writeSync(1, `捕获到异常：${err}\n`);
+});
+
+
 
 parseString(text, function (err, result) {
     console.log(result);
-    // var obj = JSON.parse(result); // syntax error
-    // console.log("id: " + obj.title);
+    var obj = JSON.parse(result); // runtime exception
+     console.log("id: " + obj.title);
 });
+
+
 
 
