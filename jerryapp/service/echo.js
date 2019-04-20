5ef94d0641fc3cd3807722e491dfae9a9ef6bee2
ChenMrChen <48763291+ChenMrChen@users.noreply.github.com>
2019-04-21 00:06:13 +0800

    Update echo.js

--- jerryapp/service/echo.js
+++ jerryapp/service/echo.js
@@ -2,7 +2,7 @@ var replyMessage = require("../tool/replyMessage.js");
 
 module.exports = function(wholecontent, question, res){
 
-  var reply = "Add by Jerry: " + question; 
+  var reply = "Add by Mr Chen: " + question; 
   var xml = replyMessage(wholecontent, reply);
   res.send(xml);
 };
